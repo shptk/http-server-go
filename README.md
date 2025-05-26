e89a574a5472b4be19ebb74ac9133d3976ab88f8
shptk <shashwatpathak100@gmail.com>
2025-05-26 17:35:51 +0000

    updated readme and now my server gives response body
    now the server not just only listens but gives a response body

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a http-server written in go.
+After going through go basics, and working on backend development projects in my college days in flask 3 years back, it is time to come back and polish my skillsets.
+
+Always had something for networking concepts, backend development and how things actually works under the hood.
+Therefore, writing this go server from scratch, without http module initially, to get an understanding of go concepts and how things work under the hood actually in these layers.
+
+Currently in the profile of DevOps, Infra or Platform, I can already see getting better grasp of what's happening in the abstraction and helping me become not just a better developer which I always wanted, but a better debugger which is what my current profile requires.
--- app/main.go
+++ app/main.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("Starting the server....")
 	l, err := net.Listen("tcp", "0.0.0.0:1234")
 	if err != nil {
-		fmt.Println("Error while setting up the tcp server: ",err)
+		fmt.Println("Error while setting up the tcp server: ", err)
 		os.Exit(1)
 	}
 
@@ -34,15 +34,15 @@ func main() {
 			req := string(buffer[:n])
 			if strings.Contains(req, "/echo") {
 				path := strings.Split(strings.Split(req, CRLF)[0][len("GET /echo"):], " ")[0]
-				fmt.Println("printing path: ",path)
-				res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",len(path),path)
+				fmt.Println("printing path: ", path)
+				res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path), path)
 
 			} else if strings.HasPrefix(req, "GET / HTTP/1.1") {
 				res = "HTTP/1.1 200 OK\r\n\r\n"
 			} else {
 				res = "HTTP/1.1 404 Not Found\r\n\r\n"
 			}
-		
+
 		}
 		res_code, err := conn.Write([]byte(res))
 		if err != nil {
